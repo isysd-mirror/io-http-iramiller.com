1b97bab7c3d954d1a6eb3dd9fadbf6a72e3fedac
Ira Miller <public@iramiller.com>
2018-07-20 11:04:26 -0500

    ledgers

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function toggleLedgers () {
   for (var i = 0; i < els.length; i++) {
     disp = disp || els[i].style.display
     if (disp === 'block') els[i].style.display = 'none'
-    else els[i].style.display = 'none'
+    else els[i].style.display = 'block'
   }
 }
 
